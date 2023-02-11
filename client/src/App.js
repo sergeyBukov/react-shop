aa446384c010d2b9b57030f45d195a8d7d6d45af
aborushko <o_xx@list.ru>
2023-02-11 21:26:58 +0300

    create template shop

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+
+            <main className='App__main'>
+
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './header.scss'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='header__content container'>
+                <div>block</div>
+                <div>block</div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
