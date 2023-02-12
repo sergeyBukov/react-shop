4cb8c33446c8aac04afa4a37e862d6f0c04693cc
aborushko <o_xx@list.ru>
2023-02-12 03:19:42 +0300

    create modal sing in

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,26 @@
 import './header.scss';
+import { useState } from 'react';
+import SingIn from '../SingIn/SingIn';
+import loginIcon from '../../images/loginIcon.svg';
 
 const Header = () => {
+
+    const [activeModal, setActiveModal] = useState(false);
+
     return (
         <header className='header'>
             <div className='header__content container'>
                 <div>block</div>
-                <div>block</div>
+
+                <div>
+                    <button className='header-login' onClick={() => setActiveModal(!activeModal)}>
+                        <img src={loginIcon} alt="Login" />
+                        <span className='header-login__text'>Login</span>
+                    </button>
+                </div>
             </div>
+
+            <SingIn active={activeModal} setActive={setActiveModal} />
         </header>
     )
 }
